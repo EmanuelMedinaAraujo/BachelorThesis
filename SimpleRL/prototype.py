6c69da836e40abd689799e05c9b7961297add604
Emanuel Medina <emanuel.medina@tum.de>
2024-07-29 14:19:59 +0200

    Code clean up

--- SimpleRL/prototype.py
+++ SimpleRL/prototype.py
@@ -1,22 +1,14 @@
-import os
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from SimpleRL.ParameterDataset import CustomParameterDataset
 from DataGeneration.util import dh_conventions
 
-max_accuracy = 0.0
-max_epoche = -1
+MODEL_SAVE_PATH = "ModelSaves/model_prototype1.pth"
+
 learning_rate = 1e-3
 batch_size = 64
 epochs = 40
-
-# DH Parameter of 2 Link Planar Robot with extended arm (alpha, a, d, theta)
-DH_EXAMPLE = torch.tensor([
-    [0, 15, 0, 0],
-    [0, 10, 0, 0]
-])
-
 device = (
     "cuda"
     if torch.cuda.is_available()
@@ -24,11 +16,9 @@ device = (
     if torch.backends.mps.is_available()
     else "cpu"
 )
-print(f"Using {device} device")
 
-
-train_dataloader = DataLoader(CustomParameterDataset(), batch_size=64, shuffle=True)
-test_dataloader = DataLoader(CustomParameterDataset(), batch_size=64, shuffle=True)
+max_accuracy = 0.0
+max_epoch = -1
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -42,24 +32,21 @@ class NeuralNetwork(nn.Module):
             nn.Linear(8, 2),
         )
 
-    def forward(self, input):
-        param, goal = input
+    def forward(self, model_input):
+        param, goal = model_input
 
         # Flatten the param
-        flattenParam = self.flatten(param)
-        # Expected output: flattenParam = torch.Size([64, 8]) and goal = torch.Size([64, 2])
+        # Expected output: flatten_param = torch.Size([64, 8]) and goal = torch.Size([64, 2])
+        flatten_param = self.flatten(param)
 
-        # Concatenate flattenParam and goal along the second dimension (dim=1)
-        flattenInput = torch.cat((flattenParam, goal), dim=1)
-        # Expected output: flattenInput shape should be torch.Size([64, 10])
+        # Concatenate flatten_param and goal along the second dimension
+        # Expected output: flatten_input shape should be torch.Size([64, 10])
+        flatten_input = torch.cat((flatten_param, goal), dim=1)
 
-        logits = self.linear_relu_stack(flattenInput)
+        logits = self.linear_relu_stack(flatten_input)
         return logits
-    
-model = NeuralNetwork().to(device)
-    
 
-def train_loop(dataloader, model, loss_fn, optimizer):
+def train_loop(dataloader, model, optimizer):
     size = len(dataloader.dataset)
     model.train()
     for batch, (param, goal) in enumerate(dataloader):
@@ -75,9 +62,9 @@ def train_loop(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), batch * batch_size + len(param)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-def test_loop(dataloader, model, t, loss_fn):
+def test_loop(dataloader, model, t):
     global max_accuracy
-    global max_epoche
+    global max_epoch
     model.eval()
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -89,58 +76,61 @@ def test_loop(dataloader, model, t, loss_fn):
             pred = model((param, goal))
             test_loss += loss_fn(param, pred, goal).item()
 
-            adaptedParam = param.clone()
-            adaptedParam[:, :, 3] = pred
+            adapted_param = param.clone()
+            adapted_param[:, :, 3] = pred
             for i in range(param.shape[0]):
-                end_effector_pos = calculate_2D_end_effector_position(adaptedParam[i])
+                end_effector_pos = calculate_2d_end_effector_position(adapted_param[i])
                 if torch.square(end_effector_pos - goal[i]).sum().sqrt() < 0.5 :
                     correct += 1
 
     test_loss /= num_batches
     correct /= size
     accuracy = 100 * correct
-    print(f"Test Error: \n Accuracy: {(accuracy):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    if(accuracy > max_accuracy):
+    print(f"Test Error: \n Accuracy: {accuracy :>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    if accuracy > max_accuracy:
         max_accuracy = accuracy
-        max_epoche = t
-        #torch.save(model, "Models/model_prototype1.pth")
+        max_epoch = t
+        #torch.save(model, MODEL_SAVE_PATH)
 
 def loss_fn(param, pred, goal):
-    batch_size = param.shape[0]
     dh_param = param.clone()
     dh_param[:, :, 3] = pred
 
     end_effector_positions = []
     for i in range(batch_size):
-        end_effector_positions.append(calculate_2D_end_effector_position(dh_param[i]))
+        end_effector_positions.append(calculate_2d_end_effector_position(dh_param[i]))
     
     end_effector_positions = torch.stack(end_effector_positions)
-    # Calculate for each row the mse loss and return the mean
 
+    # Calculate for each row the mse loss and return the mean
     distance_loss = torch.zeros(end_effector_positions.shape[0], device=device)
     for i in range(end_effector_positions.shape[0]):
         distance_loss[i] = torch.square(end_effector_positions[i] - goal[i]).sum().sqrt()
     return distance_loss.mean()
 
 
-def calculate_2D_end_effector_position(dh_param):
-    forwardKinematics = dh_conventions.dh_to_homogeneous(dh_param)
+def calculate_2d_end_effector_position(dh_param):
+    forward_kinematics = dh_conventions.dh_to_homogeneous(dh_param)
     final_transform = torch.eye(4, device=device)
-    for i in range(forwardKinematics.shape[0]):
-        final_transform = torch.matmul(final_transform, forwardKinematics[i])
+    for i in range(forward_kinematics.shape[0]):
+        final_transform = torch.matmul(final_transform, forward_kinematics[i])
 
-    end_effector_position_3D = final_transform[:3, 3]
+    end_effector_position_3d = final_transform[:3, 3]
 
-    return end_effector_position_3D[:2]
+    return end_effector_position_3d[:2]
 
+def train_model():
+    print(f"Using {device} device")
+    model = NeuralNetwork().to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    #model = torch.load(MODEL_SAVE_PATH)
+    train_dataloader = DataLoader(CustomParameterDataset(), batch_size, shuffle=True)
+    test_dataloader = DataLoader(CustomParameterDataset(), batch_size, shuffle=True)
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_dataloader, model, optimizer)
+        test_loop(test_dataloader, model, t)
+    print("The max accuracy is in Epoch {} with the value {}".format(max_epoch + 1, max_accuracy))
+    #torch.save(model, MODEL_SAVE_PATH)
 
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-#model = torch.load("Models/model_prototype1.pth")
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loop(test_dataloader, model, t, loss_fn)
-print("The max accuracy is in Epoche {} with the value {}".format(max_epoche+1, max_accuracy))
-#torch.save(model, "Models/model_prototype1.pth")
-
-print("Done!")
+    print("Done!")
--- Visualization/PlanarRobotVis.py
+++ Visualization/PlanarRobotVis.py
@@ -1,42 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import torch
 import math
 
 FRAME_SIZE_SCALAR = 1.1
 
-def plot_planar_robot(dh_parameter, standard_size=False, show_title=True):
+def visualize_planar_robot(dh_parameter, standard_size=False, show_title=True):
     # General settings
     fig, ax = plt.subplots()
-    ax.set_aspect('equal')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.grid(True)
     ax.axhline(0, color='black', lw=1.5)
     ax.axvline(0, color='black', lw=1.5)
+
     if show_title :
         plt.title('Planar Robot')
 
     # Plot the planar robot
-    start_coordinates = np.array([0, 0])
-    max_length = 0.0
-    total_angle = 0.0
-    for i in range(dh_parameter.shape[0]):
-        link_length = dh_parameter[i, 1]
-        max_length += link_length
-
-        link_angle = dh_parameter[i, 3]
-        total_angle += link_angle
-
-        end_coordinates = start_coordinates + np.array([link_length * math.cos(total_angle), link_length * math.sin(total_angle)])
-
-        # Plot link
-        ax.plot([start_coordinates[0], end_coordinates[0]], [start_coordinates[1], end_coordinates[1]], 'r-', lw=2)
-        joint_label = f'$\\theta$={dh_parameter[i, 3]:.1f}, L={dh_parameter[i, 1]:.2f}'
-        ax.plot(start_coordinates[0], start_coordinates[1], '-o', label=joint_label)
-        start_coordinates = end_coordinates
+    max_length = plot_planar_robot(ax, dh_parameter)
 
     # Set the plot size
+    set_plot_limits(ax, max_length, standard_size)
+
+    ax.legend(loc='center right', bbox_to_anchor=(1, 0.5))
+    plt.tight_layout()
+    plt.show()
+
+
+def set_plot_limits(ax, max_length, standard_size):
     if standard_size:
         # Ensure that the entire robot is visible by setting the limits as the maximum length of the robot
         # Show the same size for every arm position
@@ -48,17 +39,34 @@ def plot_planar_robot(dh_parameter, standard_size=False, show_title=True):
         x_max_limit = abs(max(ax.get_xlim(), key=abs)) * FRAME_SIZE_SCALAR
         ax.set_xlim(-x_max_limit, x_max_limit)
 
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.tight_layout()
-    plt.show()
 
+def plot_planar_robot(ax, dh_parameter):
+    start_coordinates = np.array([0, 0])
+    max_length = 0.0
+    total_angle = 0.0
+    for i in range(dh_parameter.shape[0]):
+        link_length = dh_parameter[i, 1]
+        max_length += link_length
 
+        link_angle = dh_parameter[i, 3]
+        total_angle += link_angle
+
+        end_coordinates = start_coordinates + np.array(
+            [link_length * math.cos(total_angle), link_length * math.sin(total_angle)])
+
+        # Plot link
+        ax.plot([start_coordinates[0], end_coordinates[0]], [start_coordinates[1], end_coordinates[1]], 'r-', lw=2)
+        joint_label = f'$\\theta$={dh_parameter[i, 3]:.1f}, L={dh_parameter[i, 1]:.2f}'
+        ax.plot(start_coordinates[0], start_coordinates[1], '-o', label=joint_label)
+        start_coordinates = end_coordinates
+    return max_length
 
 # DH Parameter of 2 Link Planar Robot (alpha, a, d, theta)
 # In the planar case only a and theta are relevant
-DH_EXAMPLE = torch.tensor([
-    [0, 50, 0, np.pi/2],
-    [0, 10, 0, np.pi/2],
-    [0, 10, 0, 0],
-])
-plot_planar_robot(DH_EXAMPLE)
+# import torch
+# DH_EXAMPLE = torch.tensor([
+#     [0, 50, 0, np.pi/2],
+#     [0, 10, 0, np.pi/2],
+#     [0, 10, 0, 0],
+# ])
+# plot_planar_robot(DH_EXAMPLE)
