15abc4bf8ea0c3a92db729cea47466d5508dcf22
Emanuel Medina <emanuel.medina@tum.de>
2024-07-29 14:25:28 +0200

    Parameter Dataset code clean up

--- SimpleRL/ParameterDataset.py
+++ SimpleRL/ParameterDataset.py
@@ -1,20 +1,18 @@
-import os
-import pandas as pd
 from torch.utils.data import Dataset
 
-from DataGeneration import DH_2L_Generator as dh_gen
+from DataGeneration import DH_2L_Generator as dhGen
 
 class CustomParameterDataset(Dataset):
-    def __init__(self, randomDataLength=10000, transform=None, target_transform=None):
+    def __init__(self, random_data_length=10000):
         self.dh_parameters = []
         self.goal = []
-        for i in range(randomDataLength):
-            dh_param = dh_gen.generate_extended_planar_2_link_dh()
+        for i in range(random_data_length):
+            dh_param = dhGen.generate_extended_planar_2_link_dh()
             self.dh_parameters.append(dh_param)
-            goal = dh_gen.generate_achievable_goal(dh_param)
+            goal = dhGen.generate_achievable_goal(dh_param)
             self.goal.append(goal)
-        self.transform = transform
-        self.target_transform = target_transform
+        # self.transform = transform
+        # self.target_transform = target_transform
 
     def __len__(self):
         return len(self.dh_parameters)
@@ -22,8 +20,8 @@ class CustomParameterDataset(Dataset):
     def __getitem__(self, idx):
         param = self.dh_parameters[idx]
         goal = self.goal[idx]
-        if self.transform:
-            input = self.transform(input)
-        if self.target_transform:
-            target = self.target_transform(target)
+        # if self.transform:
+        #     input = self.transform(param, goal)
+        # if self.target_transform:
+        #     target = self.target_transform(target)
         return param, goal
