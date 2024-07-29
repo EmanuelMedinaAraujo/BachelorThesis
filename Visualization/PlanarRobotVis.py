d96a5d02e17e12678e890463db9730632f487885
Emanuel Medina <emanuel.medina@tum.de>
2024-07-29 13:55:17 +0200

    Small improvements to Visualizer and code clean up

--- Visualization/PlanarRobotVis.py
+++ Visualization/PlanarRobotVis.py
@@ -1,17 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import math
 
-# DH Parameter of 2 Link Planar Robot with extended arm (alpha, a, d, theta)
-DH_EXAMPLE = torch.tensor([
-    [0, 50, 0, np.pi/2],
-    [0, 10, 0, 0]
-])
-DH_EXAMPLE_NUM_LINKS = 2
+FRAME_SIZE_SCALAR = 1.1
 
-# Because the robot is planar, we can ignore d and alpha from the DH parameters
-
-def plot_planar_robot(dh_parameter, show_link_info=True):
+def plot_planar_robot(dh_parameter, standard_size=False, show_title=True):
+    # General settings
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
     ax.set_xlabel('X')
@@ -19,8 +14,10 @@ def plot_planar_robot(dh_parameter, show_link_info=True):
     ax.grid(True)
     ax.axhline(0, color='black', lw=1.5)
     ax.axvline(0, color='black', lw=1.5)
-    fig.set_size_inches(10,10)
+    if show_title :
+        plt.title('Planar Robot')
 
+    # Plot the planar robot
     start_coordinates = np.array([0, 0])
     max_length = 0.0
     total_angle = 0.0
@@ -31,23 +28,37 @@ def plot_planar_robot(dh_parameter, show_link_info=True):
         link_angle = dh_parameter[i, 3]
         total_angle += link_angle
 
-        end_coordinates = start_coordinates + np.array([link_length * np.cos(total_angle), link_length * np.sin(total_angle)])
+        end_coordinates = start_coordinates + np.array([link_length * math.cos(total_angle), link_length * math.sin(total_angle)])
 
         # Plot link
         ax.plot([start_coordinates[0], end_coordinates[0]], [start_coordinates[1], end_coordinates[1]], 'r-', lw=2)
-        ax.plot(start_coordinates[0], start_coordinates[1], 'bo')
-
-        if show_link_info:
-            degree = np.rad2deg(total_angle)
-            ax.text(start_coordinates[0].item(), start_coordinates[1].item(), f'Joint {i + 1}\n{link_length:.2f}\n{degree:.2f}',fontsize=12)
-
+        joint_label = f'$\\theta$={dh_parameter[i, 3]:.1f}, L={dh_parameter[i, 1]:.2f}'
+        ax.plot(start_coordinates[0], start_coordinates[1], '-o', label=joint_label)
         start_coordinates = end_coordinates
 
-    # Ensure that the entire robot is visible by setting the limits as the maximum length of the robot
-    max_length *= 1.1
-    ax.set_xlim(-max_length, max_length)
-    ax.set_ylim(-max_length, max_length)
+    # Set the plot size
+    if standard_size:
+        # Ensure that the entire robot is visible by setting the limits as the maximum length of the robot
+        # Show the same size for every arm position
+        max_length *= FRAME_SIZE_SCALAR
+        ax.set_xlim(-max_length, max_length)
+        ax.set_ylim(-max_length, max_length)
+    else:
+        # Only show relevant part with a robot arm
+        x_max_limit = abs(max(ax.get_xlim(), key=abs)) * FRAME_SIZE_SCALAR
+        ax.set_xlim(-x_max_limit, x_max_limit)
+
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.tight_layout()
     plt.show()
 
 
+
+# DH Parameter of 2 Link Planar Robot (alpha, a, d, theta)
+# In the planar case only a and theta are relevant
+DH_EXAMPLE = torch.tensor([
+    [0, 50, 0, np.pi/2],
+    [0, 10, 0, np.pi/2],
+    [0, 10, 0, 0],
+])
 plot_planar_robot(DH_EXAMPLE)
