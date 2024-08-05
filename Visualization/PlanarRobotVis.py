9cfa690989d7de124f6bbab4535d31a2d121ef9d
Emanuel Medina <emanuel.medina@tum.de>
2024-08-05 13:16:30 +0200

    Improved visualization

--- Visualization/PlanarRobotVis.py
+++ Visualization/PlanarRobotVis.py
@@ -2,9 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-FRAME_SIZE_SCALAR = 1.1
+from Tests.DH_DataLoaderTest import dh_param
 
-def visualize_planar_robot(dh_parameter, standard_size=False, show_title=True):
+FRAME_SIZE_SCALAR = 1.1
+DEFAULT_PROBABILITY = 2
+
+def visualize_planar_robot(dh_parameter, link_accuracy=None, standard_size=False, show_title=True):
+    if link_accuracy == None :
+        # Create a tensor of length dh_parameter.shape[0] with DEFAULT_PROBABILITY as values
+        link_accuracy = torch.full((dh_parameter.shape[0],), DEFAULT_PROBABILITY)
+    else:
+        assert dh_parameter.shape[0] == link_accuracy.shape[0], "The amount of robots arm and the amount of probabilities is not equal"
+
     # General settings
     fig, ax = plt.subplots()
     ax.set_xlabel('X')
@@ -16,9 +25,11 @@ def visualize_planar_robot(dh_parameter, standard_size=False, show_title=True):
     if show_title :
         plt.title('Planar Robot')
 
-    # Plot the planar robot
-    max_length = plot_planar_robot(ax, dh_parameter)
-
+    max_length = 0
+    for i in range(dh_parameter.shape[0]):
+        # Plot the planar robot
+        arm_length = plot_planar_robot(ax, dh_parameter[i], link_accuracy[i])
+        max_length = max(arm_length, max_length)
     # Set the plot size
     set_plot_limits(ax, max_length, standard_size)
 
@@ -26,21 +37,7 @@ def visualize_planar_robot(dh_parameter, standard_size=False, show_title=True):
     plt.tight_layout()
     plt.show()
 
-
-def set_plot_limits(ax, max_length, standard_size):
-    if standard_size:
-        # Ensure that the entire robot is visible by setting the limits as the maximum length of the robot
-        # Show the same size for every arm position
-        max_length *= FRAME_SIZE_SCALAR
-        ax.set_xlim(-max_length, max_length)
-        ax.set_ylim(-max_length, max_length)
-    else:
-        # Only show relevant part with a robot arm
-        x_max_limit = abs(max(ax.get_xlim(), key=abs)) * FRAME_SIZE_SCALAR
-        ax.set_xlim(-x_max_limit, x_max_limit)
-
-
-def plot_planar_robot(ax, dh_parameter):
+def plot_planar_robot(ax, dh_parameter, link_accuracy = 2):
     start_coordinates = np.array([0, 0])
     max_length = 0.0
     total_angle = 0.0
@@ -55,18 +52,39 @@ def plot_planar_robot(ax, dh_parameter):
             [link_length * math.cos(total_angle), link_length * math.sin(total_angle)])
 
         # Plot link
-        ax.plot([start_coordinates[0], end_coordinates[0]], [start_coordinates[1], end_coordinates[1]], 'r-', lw=2)
+        ax.plot([start_coordinates[0], end_coordinates[0]], [start_coordinates[1], end_coordinates[1]], 'r-', lw=link_accuracy)
         joint_label = f'$\\theta$={dh_parameter[i, 3]:.1f}, L={dh_parameter[i, 1]:.2f}'
         ax.plot(start_coordinates[0], start_coordinates[1], '-o', label=joint_label)
         start_coordinates = end_coordinates
     return max_length
 
+def set_plot_limits(ax, max_length, standard_size):
+    if standard_size:
+        # Ensure that the entire robot is visible by setting the limits as the maximum length of the robot
+        # Show the same size for every arm position
+        max_length *= FRAME_SIZE_SCALAR
+        ax.set_xlim(-max_length, max_length)
+        ax.set_ylim(-max_length, max_length)
+    else:
+        # Only show relevant part with a robot arm
+        x_max_limit = abs(max(ax.get_xlim(), key=abs)) * FRAME_SIZE_SCALAR
+        ax.set_xlim(-x_max_limit, x_max_limit)
+
 # DH Parameter of 2 Link Planar Robot (alpha, a, d, theta)
 # In the planar case only a and theta are relevant
-# import torch
-# DH_EXAMPLE = torch.tensor([
-#     [0, 50, 0, np.pi/2],
-#     [0, 10, 0, np.pi/2],
-#     [0, 10, 0, 0],
-# ])
-# plot_planar_robot(DH_EXAMPLE)
+import torch
+DH_EXAMPLE = torch.tensor([
+    [[0, 50, 0, np.pi/2],
+    [0, 10, 0, np.pi/2],
+    [0, 10, 0, 0]
+    ],
+
+    [[0, 50, 0, np.pi/4],
+    [0, 10, 0, np.pi/4],
+    [0, 10, 0, 0]
+    ],
+])
+accuracy= torch.tensor([0.5, 2])
+
+#visualize_planar_robot(DH_EXAMPLE)
+visualize_planar_robot(DH_EXAMPLE, accuracy)
