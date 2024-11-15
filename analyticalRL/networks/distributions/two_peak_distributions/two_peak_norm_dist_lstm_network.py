2e45a11d49177403d77074ac643729cab4644b40
Emanuel Medina <emanuel.medina@tum.de>
2024-11-15 01:53:56 +0100

    Adapted lstm raw variant and prepared server execution

--- analyticalRL/networks/distributions/two_peak_distributions/two_peak_norm_dist_lstm_network.py
+++ analyticalRL/networks/distributions/two_peak_distributions/two_peak_norm_dist_lstm_network.py
@@ -12,12 +12,12 @@ class TwoPeakNormalLstmDistrNetwork(KinematicsNetworkBase):
 
     def __init__(self, num_joints, num_layer, layer_sizes, logger, error_tolerance):
         super().__init__(num_joints, num_layer, layer_sizes, logger, error_tolerance)
-        self.hidden_size = num_joints * 6  # number of features in hidden state
-        self.num_layers = 6  # number of stacked LSTM layers
-        self.output_size = num_joints * 6
-        self.lstm = nn.LSTM(input_size=num_joints * 3 + 2, hidden_size=self.hidden_size,
-                            num_layers=self.num_layers, batch_first=True)
-        self.relu = nn.ReLU()
+        self.hidden_size = 512  # number of features in hidden state
+        self.input_size = num_joints * 3 + 2
+        self.num_layers = 1  # number of stacked LSTM layers
+        self.proj_size = num_joints * 6
+        self.lstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size,
+                            num_layers=self.num_layers, proj_size=self.proj_size, batch_first=True)
 
     def forward(self, model_input):
         flatten_input = super().flatten_model_input(model_input)
@@ -27,15 +27,15 @@ class TwoPeakNormalLstmDistrNetwork(KinematicsNetworkBase):
         flatten_input = flatten_input.unsqueeze(0 if is_single_parameter else 1 )
 
         if is_single_parameter:
-            h_0 = Variable(torch.zeros(self.num_layers, self.hidden_size)).to(param.device)
+            h_0 = Variable(torch.zeros(self.num_layers, self.proj_size)).to(param.device)
             c_0 = Variable(torch.zeros(self.num_layers, self.hidden_size)).to(param.device)
         else:
-            h_0 = Variable(torch.zeros(self.num_layers, flatten_input.size(0), self.hidden_size)).to(param.device)
+            h_0 = Variable(torch.zeros(self.num_layers, flatten_input.size(0), self.proj_size)).to(param.device)
             c_0 = Variable(torch.zeros(self.num_layers, flatten_input.size(0), self.hidden_size)).to(param.device)
         # Propagate input through LSTM
         output, _ = self.lstm(flatten_input, (h_0, c_0))  # lstm with input, hidden, and internal state
         out = output.squeeze(1)  # reshaping the data for Dense layer next
-        out = torch.sigmoid(out)
+        out = torch.sigmoid(out) # ne´cessary or errors (TODO debug)
         network_output = NormalizeWeightsLayer(self.num_joints)(out)
 
         if is_single_parameter:
