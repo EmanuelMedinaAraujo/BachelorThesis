9542a5458a3783854eee7d279b8559048e1b9437
Emanuel Medina <emanuel.medina@tum.de>
2024-11-24 20:03:01 +0100

    Prepare 2peak lstm optuna

--- main.py
+++ main.py
@@ -33,15 +33,15 @@ def main(train_config: TrainConfig):
     num_processes = train_config.optuna.num_processes
     num_trials_per_process = train_config.optuna.trials_per_process
     try:
-        optuna.delete_study(study_name='distribution_optuna_1p_lstm',
-                            storage=f'sqlite:///distribution_optuna_1p_lstm.db')
+        optuna.delete_study(study_name='distribution_optuna_2p_lstm',
+                            storage=f'sqlite:///distribution_optuna_2p_lstm.db')
     except KeyError:
         pass
     study = optuna.create_study(sampler=optuna.samplers.TPESampler(),
                                 pruner=optuna.pruners.MedianPruner(n_warmup_steps=minimal_steps),
                                 direction='maximize' if train_config.use_stb3 else 'minimize',
-                                study_name='distribution_optuna_1p_lstm',
-                                storage=f'sqlite:///distribution_optuna_1p_lstm.db',
+                                study_name='distribution_optuna_2p_lstm',
+                                storage=f'sqlite:///distribution_optuna_2p_lstm.db',
                                 )
 
     arguments = [(study, copy_cfg(train_config), num_trials_per_process) for _ in range(num_processes)]
@@ -134,10 +134,9 @@ def _objective(defaults: TrainConfig, trial: optuna.Trial):
 
         # noinspection SpellCheckingInspection
         cfg_copy.hyperparams.analytical.optimizer = trial.suggest_categorical('optimizer', ['Adam', 'SGD', 'RMSprop'])
-        # cfg_copy.hyperparams.analytical.output_type = trial.suggest_categorical('output_type',
-        #                                                                         ['NormalDistrGroundTruthLossNetwork',
-        #                                                                          'NormalDistrManualReparameterizationNetwork',
-        #                                                                          'NormalDistrRandomSampleDistNetwork'])
+        cfg_copy.hyperparams.analytical.output_type = trial.suggest_categorical('output_type',
+                                                                                ['TwoPeakNormalLstmDistrNetwork',
+                                                                                 'TwoPeakNormalLstmVariantDistrNetwork'])
 
         # lstm hyperparameters
         if 'lstm' in cfg_copy.hyperparams.analytical.output_type.lower():
